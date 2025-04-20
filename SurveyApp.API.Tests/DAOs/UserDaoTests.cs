be2c1dc2aa6a2b9e24ed84edce49012aaaed6f1c
barathrum54 <tahabdurmus0@gmail.com>
2025-04-20 17:46:42 +0300

    test: add unit test for UserDao.GetByUsername with existing user

---
+++ SurveyApp.API.Tests/DAOs/UserDaoTests.cs
@@ -0,0 +1,40 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SqlBatis.DataMapper.DependencyInjection;
+using SurveyApp.API.DAOs;
+using SurveyApp.API.DAOs.Interfaces;
+
+namespace SurveyApp.API.Tests.DAOs;
+
+public class UserDaoTests
+{
+  private readonly IUserDao _userDao;
+
+  public UserDaoTests()
+  {
+    var host = Host.CreateDefaultBuilder()
+        .ConfigureServices((context, services) =>
+        {
+          services.AddSqlMapper(options =>
+                  context.Configuration.GetSection("DB").Bind(options));
+          services.AddScoped<IUserDao, UserDao>();
+        })
+        .Build();
+
+    _userDao = host.Services.GetRequiredService<IUserDao>();
+  }
+
+  [Fact]
+  public void GetByUsername_ShouldReturn_AdminUser()
+  {
+    // Act
+    var user = _userDao.GetByUsername("admin");
+
+    // Assert
+    Assert.NotNull(user);
+    Assert.Equal("admin", user.Username);
+    Assert.Equal("admin1234", user.PasswordHash);
+    Assert.True(user.Id > 0);
+  }
+}
